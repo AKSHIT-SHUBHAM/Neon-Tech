6cc38e5674dba0b6e172f028a4aaf24ae1478b1b
neon-tech <65966279+AKSHIT-SHUBHAM@users.noreply.github.com>
2020-12-21 18:20:27 +0530

    first_commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var material_btn_1 = document.getElementById("akshit");
+material_btn_1.addEventListener('mouseover', function() {
+	document.new();
+	document.write("<b>hi</B>");
+	document.close();
+}
